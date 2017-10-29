35071e8c17be92691de37bbfeabae8810b8b668f
ajblackie <ajblackie@hotmail.com>
2017-10-28 19:55:07 -0500

    changed row push from null for better formatting

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,3 +1,4 @@
+// Generate player board by number of rows and columns
 const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
   let board = [];
   for (let rows = 0; rows < numberOfRows; rows++) {
@@ -9,3 +10,37 @@ const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
   }
   return board;
 };
+
+// Generate bomb board
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let board = [];
+  for (let rows = 0; rows < numberOfRows; rows++) {
+    let row = [];
+    for (let columns = 0; columns < numberOfColumns; columns++) {
+      row.push(' '); // I like this better than null
+    }
+    board.push(row);
+  }
+  let numberOfBombsPlaced = 0;
+  while (numberOfBombsPlaced < numberOfBombs) {
+    let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+    board[randomRowIndex][randomColumnIndex] = 'B';
+    numberOfBombsPlaced++;
+    //need to account for overlapping bombs with control flow
+  }
+
+  return board;
+}
+
+const printBoard = board => {
+  console.log(board.map(row => row.join(' | ')).join('\n'));
+}
+
+let playerBoard = generatePlayerBoard(3, 4);
+let bombBoard = generateBombBoard(3, 4, 5);
+
+console.log('Player Board: ');
+printBoard(playerBoard);
+console.log('Bomb Board: ');
+printBoard(bombBoard);
