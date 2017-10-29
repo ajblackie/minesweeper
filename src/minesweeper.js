45e28dad1342a7011561c65c340d1288dc214808
ajblackie <ajblackie@hotmail.com>
2017-10-28 19:16:33 -0500

    dynamically generate board by user input for rows and columns

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,17 +1,11 @@
-const printBoard = board => {
-  console.log('Current Board:');
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  let board = [];
+  for (let rows = 0; rows < numberOfRows; rows++) {
+    let row = [];
+    for (let columns = 0; columns < numberOfColumns; columns++) {
+      row.push(' ');
+    }
+    board.push(row);
+  }
+  return board;
 };
-const board = [
-  [' ', ' ', ' '],
-  [' ', ' ', ' '],
-  [' ', ' ', ' ']
-];
-printBoard(board);
-
-board[0][1] = '1';
-board[2][2] = 'B';
-
-printBoard(board);
