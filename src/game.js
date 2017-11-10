2c5816338b82b7cd5d85b693789c5d21ae2100ea
ajblackie <ajblackie@hotmail.com>
2017-11-09 20:50:08 -0600

    fix board is undefined and re-transpiled

--- src/game.js
+++ src/game.js
@@ -1,14 +1,4 @@
-// To play Minesweeper, we will create instances of MineSweeperGame in command line.
-// For example:
-// In the command line, navigate to the lib directory and run `node`
-// Run `.load game.js` to load the contents of this file.
-// Then create a Game instance and run commands like so:
-// let game = new Game(3, 3, 3);
-// game.playMove(0, 1);
-// game.playMove(1, 2);
-// When done run `.exit`
-
-import {board} from './board';
+import {Board} from './board';
 
 class Game {
     constructor(numberOfRows, numberOfColumns, numberOfBombs) {
@@ -29,3 +19,13 @@ class Game {
       }
     }
   }
+
+// To play Minesweeper, we will create instances of MineSweeperGame in command line.
+// For example:
+// In the command line, navigate to the lib directory and run `node`
+// Run `.load game.js` to load the contents of this file.
+// Then create a Game instance and run commands like so:
+// let game = new Game(3, 3, 3);
+// game.playMove(0, 1);
+// game.playMove(1, 2);
+// When done run `.exit`
